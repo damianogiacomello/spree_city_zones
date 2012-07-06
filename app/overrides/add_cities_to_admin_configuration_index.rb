1ccd3a9cb96fb902f35a9572ff66c7cdc3ddc0e3
Damiano <damiano.giacomello@diginess.it>
2012-07-06 11:47:38 +0200

    removed old override

--- app/overrides/add_cities_to_admin_configuration_index.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:name => "add_cities_to_admin_configuration_index",
-                  :virtual_path => "admin/configurations/index",
-                  :insert_bottom => "[data-hook='admin_configurations_menu']",
-                  :partial => "admin/shared/cities_admin_configurations_menu")
