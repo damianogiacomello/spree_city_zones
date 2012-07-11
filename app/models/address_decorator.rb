5a4d754f9abba0d1d97c623057877d55d7021bd1
Damiano <damiano.giacomello@diginess.it>
2012-07-11 15:44:52 +0200

    fixed checkout address validation. added city attr_accessor

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -1,6 +1,13 @@
 Address.class_eval do
   belongs_to :city
   
+  attr_accessor :city
+  before_validation :get_city_id_or_name
+  
+  def get_city_id_or_name
+    self.city = true if !self.city_id.blank? || !self.city_name.blank?
+  end
+  
   def state_text
     city.nil? ? city_name : city.name
   end
