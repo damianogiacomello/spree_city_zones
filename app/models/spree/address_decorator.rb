d0cafab57d04ed1e7904e04dfdbb03c2c30c4a47
Damiano <damiano.giacomello@diginess.it>
2012-07-11 15:46:21 +0200

    added city attr_accessor for fix checkout validation address

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,7 +1,14 @@
 Spree::Address.class_eval do
   belongs_to :city
   
+  attr_accessor :city
+  before_validation :get_city_id_or_name
+  
   attr_accessible :city_id, :city_name
+  
+  def get_city_id_or_name
+    self.city = true if !self.city_id.blank? || !self.city_name.blank?
+  end
   
   def state_text
     city.nil? ? city_name : city.name
